e84127b77a116ea0aa511e71d814d0bb0789b508
Andrea Moretti <andrea.moretti1704@gmail.com>
2025-10-14 13:00:45 +0200

    pause creating delete

--- src/components/custom-hooks/UseTasks.jsx
+++ src/components/custom-hooks/UseTasks.jsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 export default function useTasks() {
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
   const [tasks, setTasks] = useState([]);
+
+  const { id } = useParams();
 
   useEffect(() => {
     fetch(`${backendUrl}/tasks`)
@@ -27,7 +30,25 @@ export default function useTasks() {
       alert("Task aggiunta con successo!");
     }
   };
-  const removeTask = () => {};
+  //   Completare la funzione removeTask in useTasks():
+  // La funzione deve ricevere un taskId e effettuare una chiamata API DELETE /tasks/:id.
+
+  // La chiamata API restituisce un oggetto con la seguente struttura:
+
+  // In caso di successo:
+
+  // { success: true }
+  // In caso di errore:
+
+  // { success: false, message: "Messaggio di errore" }
+  // La funzione removeTask deve controllare il valore di success nella risposta:
+
+  // Se success è true, rimuovere il task dallo stato globale.
+  // Se success è false, lanciare un errore con message come testo. -->
+
+  const removeTask = async (taskId) => {
+    fetch(`${backendUrl}/task/${id}`);
+  };
   const updateTask = () => {};
   return { tasks, setTasks, addTask, removeTask, updateTask };
 }
